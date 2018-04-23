c90e89e2f4f477a0ef6d9cf61cfdaed2e2a6616b
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-04-23 18:35:50 -0400

    final code

--- main/NaiveBayes.py
+++ main/NaiveBayes.py
@@ -3,6 +3,43 @@ import random
 import csv
 from collections import Counter
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import math
+
+
+tokenize = lambda doc: doc.lower().split(" ")
+
+
+def inverse_document_frequencies(tokenized_documents):
+    idf_values = {}
+    all_tokens_set = set([item for sublist in tokenized_documents for item in sublist])
+    for tkn in all_tokens_set:
+        contains_token = map(lambda doc: tkn in doc, tokenized_documents)
+        idf_values[tkn] = 1 + math.log(len(tokenized_documents)/(sum(contains_token)))
+    return idf_values
+
+
+def word_tfidf(documents):
+
+    tokenized_documents = [tokenize(d) for d in documents]
+    idf = inverse_document_frequencies(tokenized_documents)
+    word_count = dict()
+    for document in tokenized_documents:
+        for term in idf.keys():
+            tf = sublinear_term_frequency(term, document)
+
+            if term in word_count:
+                word_count[term] += tf
+            else:
+                word_count[term] = tf
+
+    return word_count
+
+
+def sublinear_term_frequency(term, tokenized_document):
+    count = tokenized_document.count(term)
+    if count == 0:
+        return 0
+    return 1 + math.log(count)
 
 
 # splitting the dataSet into 90:10 ratio for 10-fold cross validation
@@ -86,9 +123,9 @@ def split_data_by_classes(reviews):
     return pos_data, neg_data
 
 
-def main(n, k):
-    pos_data = list(csv.reader(open('training/pos/corpus_pos.csv', "r")))
-    neg_data = list(csv.reader(open('training/neg/corpus_neg.csv', "r")))
+def naive_bayes(n, k, pos_file_path, neg_file_path, is_tfidf):
+    pos_data = list(csv.reader(open(pos_file_path, "r")))
+    neg_data = list(csv.reader(open(neg_file_path, "r")))
 
     all_data = pos_data + neg_data
     actual_labels = list()
@@ -109,8 +146,12 @@ def main(n, k):
         pos_prob = pos_total / float(total)
         neg_prob = neg_total / float(total)
 
-        neg_counts = word_count(neg_data)
-        pos_counts = word_count(pos_data)
+        if is_tfidf:
+            neg_counts = word_tfidf(neg_data)
+            pos_counts = word_tfidf(pos_data)
+        else:
+            neg_counts = word_count(neg_data)
+            pos_counts = word_count(pos_data)
 
         vocab_counts = dict(Counter(neg_counts) + Counter(pos_counts))
 
@@ -121,11 +162,11 @@ def main(n, k):
 
         test_data = data[1]
 
-        # j = random.randrange(len(test_data))
+        # test_total = len(test_data)
 
-        test_total = len(test_data)
+        for x in range(1):
 
-        for j in range(test_total):
+            j = random.randrange(len(test_data))
 
             test_review = test_data[j][1]
             test_class = test_data[j][2]
@@ -149,23 +190,34 @@ def main(n, k):
 
     evaluation(actual_labels, predicted_labels)
 
-    percentage = (correct_classification_count / float(correct_classification_count + incorrect_classification_count)) * 100
-    print "Average accuracy for k-fold cross validation split over n iterations is " + str(percentage) + "%"
-
 
 def evaluation(actual_labels, predicted_labels):
     accuracy = accuracy_score(actual_labels, predicted_labels)
     precision = precision_score(actual_labels, predicted_labels, [0, 1], '1', 'binary')
     recall = recall_score(actual_labels, predicted_labels, [0, 1], '1')
     f1 = f1_score(actual_labels, predicted_labels, [0, 1], '1')
-    str_accuracy = "%.9f" % accuracy
-    str_precision = "%.9f" % precision
-    str_recall = "%.9f" % recall
-    str_f1 = "%.9f" % f1
-    print "Accuracy: " + str_accuracy
-    print "Precision: " + str_precision
-    print "Recall: " + str_recall
-    print "F1: " + str_f1
+    str_accuracy = "%.2f" % (accuracy * 100)
+    str_precision = "%.2f" % (precision * 100)
+    str_recall = "%.2f" % (recall * 100)
+    str_f1 = "%.2f" % (f1 * 100)
+    print "Accuracy: " + str_accuracy + "%"
+    print "Precision: " + str_precision + "%"
+    print "Recall: " + str_recall + "%"
+    print "F1: " + str_f1 + "%"
 
 
-main(1, 10)
+def main():
+    print "Naive Bayes for Unigrams"
+    naive_bayes(10, 10, 'training/pos/corpus_pos.csv', 'training/neg/corpus_neg.csv', False)
+
+    print "Naive Bayes for Bigrams"
+    naive_bayes(10, 10, 'training/pos/corpus_bigram_pos.csv', 'training/neg/corpus_bigram_neg.csv', False)
+
+    print "Naive Bayes for Unigrams with TF-IDF"
+    naive_bayes(10, 10, 'training/pos/corpus_pos.csv', 'training/neg/corpus_neg.csv', True)
+
+    print "Naive Bayes for Bigrams with TF-IDF"
+    naive_bayes(10, 10, 'training/pos/corpus_bigram_pos.csv', 'training/neg/corpus_bigram_neg.csv', True)
+
+
+main()
